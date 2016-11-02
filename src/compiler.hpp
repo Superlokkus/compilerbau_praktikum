967b5d979f9048013be54def8de2b05d499c4091
Markus Klemm <markus@markus@markusklemm.net>
2016-11-02 14:27:03 +0100

    Interface for task2

--- src/compiler.hpp
+++ src/compiler.hpp
@@ -1,6 +1,8 @@
 
 #include <string>
 #include <utility>
+#include <fstream>
+#include <boost/variant.hpp>
 
 /*! @brief Simple arithmatic parsing
  *
@@ -14,4 +16,36 @@ namespace task1_grammar{
     bool expression(std::string& input, result_t& output);
     bool term(std::string& input, result_t& output);
     bool factor(std::string& input, result_t& output);
+}
+
+
+namespace PL0{
+    using string = std::string;
+    using number = int32_t;
+
+    enum struct morphem_class{
+        symbol,
+        number,
+        identifier,
+    };
+
+    struct position {
+
+    };
+
+    struct morphem{
+        boost::variant<string,number> value;
+        morphem_class morphem_class;
+        position position;
+    };
+
+    class lexer{
+    public:
+        lexer(std::fstream &file);
+        morphem lex();
+        morphem operator()() {return lex();}
+        morphem lexGetMorph() const;
+        morphem lexNextMorph() {return lex();}
+
+    };
 }
